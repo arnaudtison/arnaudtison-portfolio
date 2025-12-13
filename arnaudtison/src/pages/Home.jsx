41e0474ed982f5cf1be52fda07ffa20bb5fd061e
Arnaud Tison <arnaud.tison@student.hogent.be>
2025-12-13 21:49:57 +0100

    landing page

--- arnaudtison/src/pages/Home.jsx
+++ arnaudtison/src/pages/Home.jsx
@@ -1,8 +1,126 @@
+import { useState, useEffect } from "react";
+import logo from "../assets/logo_black.png";
+import imageSelf from "../assets/imageSelf.png";
+import { Instagram, Github } from "lucide-react";
+import "../css/home.scss";
 
 function Home() {
+  const roles = ["Full-Stack Developer", "Designer", "Student"];
+  const [currentRole, setCurrentRole] = useState(0);
+  const [displayText, setDisplayText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const role = roles[currentRole];
+    const delay = isDeleting ? 50 : 100;
+    const target = isDeleting ? "" : role;
+
+    const timeout = setTimeout(() => {
+      if (!isDeleting && displayText === role) {
+        setTimeout(() => setIsDeleting(true), 2000);
+      } else if (isDeleting && displayText === "") {
+        setCurrentRole((prev) => (prev + 1) % roles.length);
+        setIsDeleting(false);
+      } else {
+        setDisplayText(isDeleting ? displayText.slice(0, -1) : displayText + role[displayText.length]);
+      }
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }, [displayText, isDeleting, currentRole, roles]);
 
   return (
-    <div className="Home"></div>
+    <div className="page-wrapper">
+      <div className="navigation-wrapper">
+        <div className="logo-container">
+          <img src={logo} alt="arnaudtison logo" />
+        </div>
+        <div className="redirect-links-container">
+          <div className="redirect-link">WHO AM I?</div>
+          <div className="redirect-link">WORK</div>
+          <div className="redirect-link">GET IN TOUCH</div>
+          <div className="external-links-container">
+            <Instagram size={35} />
+            <Github size={35} />
+          </div>
+        </div>
+      </div>
+      <div className="landing-wrapper">
+        <div className="horizontal-slider">
+          <div className="marquee">
+            <div className="marquee__track">
+              <span className="marquee__content">
+                ARNAUD TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD
+                TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD
+                TISON&nbsp;ARNAUD TISON&nbsp;
+              </span>
+              <span className="marquee__content" aria-hidden="true">
+                ARNAUD TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD
+                TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD TISON&nbsp;ARNAUD
+                TISON&nbsp;ARNAUD TISON&nbsp;
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="content-grid">
+          <div className="syntax-block left">
+            <div className="syntax-block-input">
+              <span>java</span>
+              <div className="syntax-wrapper">
+                <p>String[] roles = {"{"}</p>
+                <p>"<b>Full-Stack Developer</b>",</p>
+                <p>"<b>Designer</b>",</p>
+                <p>"<b>Student</b>"</p>
+                <p>{"}"};</p>
+                <br />
+                <p>for (String role : roles) {"{"}</p>
+                <p>System.out.println("I am a " + role);</p>
+                <p>{"}"}</p>
+              </div>
+            </div>
+            <div className="syntax-block-output">
+              <p>{">"} portfolio.java: I am a <span className="typing-text">{displayText}</span><span className="cursor">|</span></p>
+            </div>
+          </div>
+          <div className="image-container right">
+            <img src={imageSelf} alt="Arnaud Tison" />
+          </div>
+        </div>
+        <div className="horizontal-language-slider">
+          <div className="marquee marquee--reverse">
+            <div className="marquee__track">
+              <div className="marquee__content">
+                <span>HTML</span>
+                <span>CSS</span>
+                <span>JAVA</span>
+                <span>JAVASCRIPT</span>
+                <span>TYPESCRIPT</span>
+                <span>REACT</span>
+                <span>C#</span>
+                <span>BLAZOR</span>
+                <span>PYTHON</span>
+                <span>SQL</span>
+              </div>
+
+              {/* duplicate for seamless loop */}
+              <div className="marquee__content" aria-hidden="true">
+                <span>HTML</span>
+                <span>CSS</span>
+                <span>JAVA</span>
+                <span>JAVASCRIPT</span>
+                <span>TYPESCRIPT</span>
+                <span>REACT</span>
+                <span>C#</span>
+                <span>BLAZOR</span>
+                <span>PYTHON</span>
+                <span>SQL</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <ScrollIndicator /> */}
+    </div>
   );
 }
 
